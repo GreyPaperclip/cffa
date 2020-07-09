25b9fecfd5d6ab3cfd7879ee3ee1a7d820afcc1a
GreyPaperclip <github@richardborrett.com>
2020-07-09 21:38:52 +0100

    Server.py documented.

--- server.py
+++ server.py
@@ -1,4 +1,59 @@
-""" CFFA application V0.1. Casual Football Finance Manager with multi-tenancy project
+""" CFFA server V0.2. Casual Football Finance Manager with multi-tenancy project
+
+This code file contains all endpoints for the CFFA flask web service. It requires the cffadb library integrate with a mongoDB
+bckend. Authentication is integrated with the Auth0 authentication service. Execute this code file with Flask / gunicorn.
+
+The application uses port 5000.
+
+Environment Variables
+---------------------
+
+AUTH0_CLIENT_ID=[as provided by Auth0 for this application]
+AUTH0_DOMAIN=[as provided by Auth0]
+AUTH0_CLIENT_SECRET=[as provided by Auth0 for this application]
+AUTH0_CALLBACK_URL=https://yourwebsite.com:port/callback
+AUTH0_AUDIENCE=https://[as advised by Auth0]]/userinfo
+
+BACKEND_DBPWD=[mongpDB backend DB password]
+BACKEND_DBUSR=[mongoDB backend username for football DB]
+BACKEND_DBHOST=[IP address/hostnames of mongoDB server/s]
+BACKEND_DBPORT=[Port number for MongoDB]
+BACKEND_DBNAME=[Database name, such as footballDB]
+
+SECRET_KEY=[used for CSRF/session encryption protection in forms with flask ]
+PYTHONPATH=[should include link to cffadb, altough some debate better methods could be used]
+EXPORTDIRECTORY=[Directory to use to build databasee json exports]
+
+GOOGLEKEYFILE=[Only used by testScript.py as keyfile is now uploaded server side]
+GOOGLE_SHEET=[Only used by testScript.py as gsheet name is set via cffa webpage]
+TRANSACTION_SRC_WKSHEET=[Only used by testScript.py as worksheet name is set via cffa webpage]
+GAME_SRC_WKSHEET=[Only used by testScript.py as worksheet name is set via cffa webpage]
+CFFA_USERID=[Only used by testScript.py to configure DB database from gsheet]
+SUMMARY_SRC_WKSHEET=[Only used by testScript.py as worksheet name is set via cffa webpage]
+
+Example
+-------
+
+A)
+
+export PYTHONPATH=<path to cffa>:<path to cffa db>
+export FLASK_APP=server.py ; export FLASK_ENV=development
+flask run --host 0.0.0.0 --cert your_cert.pem --key your_key.pem
+
+-- This will respond to any clients and supports SSL using either an approved or self-signed certificate in development
+mode. Do not use development mode in Production.
+
+B)
+
+#!/bin/bash
+source venv/bin/activate
+exec gunicorn -b :5000 --certfile=cffa-signed.crt \
+--keyfile=/cffa-signed.key --access-logfile - --error-logfile - -w 1 server:app
+
+Starts production class CFFA on port 5000 with logs to stdout with signed certificates. Would typically be used alongside
+a nginx reverse proxy to redirect https to port 5000. (ngninx does not decript https in this case as Auth0 authenticates
+with flask not nginx.
+
 """
 from functools import wraps
 import json
@@ -79,6 +134,9 @@ except Exception as e:
 
 @app.errorhandler(Exception)
 def handle_auth_error(ex):
+    """ Derived from Auth0 python example for single web pages. Handles authentication errors.
+
+     """
     response = jsonify(message=str(ex))
     response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
     return response
@@ -100,6 +158,10 @@ auth0 = oauth.register(
 
 
 def requires_auth(f):
+    """ Derived from Auth0 python example for single web pages. Wrapper for each entry point to ensure user is
+    authenticated.
+
+     """
     @wraps(f)
     def decorated(*args, **kwargs):
         if constants.PROFILE_KEY not in session:
@@ -109,6 +171,8 @@ def requires_auth(f):
     return decorated
 
 def requires_managerRole(f):
+    """ Additional wrap to ensure that only manager role CFFA users can access the endpoint.
+     """
     @wraps(f)
     def decorated(*args, **kwargs):
         userID = session[constants.PROFILE_KEY].get('user_id')
@@ -124,11 +188,19 @@ def requires_managerRole(f):
 # Controllers API
 @app.route('/')
 def home():
+    """ Derived from Auth0 python example for single web pages. Provides a log in button before being shown the login
+    AUth0 dialog, or if already authenticated, the entryScreen endpoint.
+
+     """
     return render_template('home.html')
 
 
 @app.route('/callback')
 def callback_handling():
+    """ Derived from Auth0 python example for single web pages. Setting up session variables prior to redirection
+    to home screen (entryScreen)
+
+     """
     auth0.authorize_access_token()
     resp = auth0.get('userinfo')
     userinfo = resp.json()
@@ -144,11 +216,15 @@ def callback_handling():
 
 @app.route('/login')
 def login():
+    """ Derived from Auth0 python example for single web pages. Shows usual Auth0 login dialog.
+     """
     return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
 
 
 @app.route('/logout')
 def logout():
+    """ Derived from Auth0 python example for single web pages. Logs out AUth0 user.
+     """
     session.clear()
     params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
@@ -157,12 +233,16 @@ def logout():
 @app.route('/dashboard')
 @requires_auth
 def dashboard():
+    """ Derived from Auth0 python example for single web pages. Not used.
+     """
     return render_template('dashboard.html',
                            userinfo=session[constants.PROFILE_KEY],
                            userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4))
 
 @app.route('/favicon.ico')
 def favicon():
+    """ Added to provide favicon file as highlighted when using chrome/safari debugger.
+    """
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
@@ -170,6 +250,8 @@ def favicon():
 @requires_auth
 @requires_managerRole
 def entryScreen():
+    """ Main screen for manager. If user collections do not exist assuems new user and redirects to onboarding screen.
+     """
     # from username lets check tenancy. Use metadata in Auth0 for userID. This will be unique.
     if ourDB.loadTeamTablesForUserId(session[constants.PROFILE_KEY].get('user_id', None)) == False:
         # new manager entry point, redirect to onboarding wizard
@@ -188,7 +270,8 @@ def entryScreen():
 @requires_auth
 @requires_managerRole
 def onboarding():
-    # display welcome screen and prompt user to create a new team name.
+    """Onboarding screen to prompt for team name for new managers.
+    """
     app.logger.debug("Entering onboarding")
     addTeamForm = formHandler.AddTeam(ourDB.getListofAllTenantNames())
 
@@ -218,6 +301,9 @@ def onboarding():
 @requires_auth
 @requires_managerRole
 def manageGames():
+    """ Functionality to manage games - add, edit, remove. The logic handles the response when adding a new game, but
+    edit and delete game are redirected to their endpoints via the form action setting in the manageGames.html template.
+    """
     # add game; edit game; delete game; dump games
     # handle guests too
     app.logger.debug('Rendering manageGames')
@@ -246,6 +332,15 @@ def manageGames():
 @requires_auth
 @requires_managerRole
 def newGame(players):
+    """ Renders and processings the new game form.
+
+    Parameters
+    ----------
+
+    players : int
+        Number of players that played in the new game. Used in form validation.
+    """
+
     app.logger.debug('Rendering newgame with players' + str(players))
     booker = session[constants.PROFILE_KEY].get('name')
     newGameDefaults = ourDB.getDefaultsForNewGame(booker)
@@ -269,6 +364,8 @@ def newGame(players):
 @requires_auth
 @requires_managerRole
 def editGame():
+    """  Processes edit game select form (ie: which game to edit).
+    """
     games = ourDB.getAllGames()
     gameLabels = formHandler.createLabelsForGames(games)
     selectGameForm = formHandler.EditGameSelectForm()
@@ -291,6 +388,14 @@ def editGame():
 @app.route('/applyEditGame/<int:choice>', methods=['GET', 'POST'])
 @requires_auth
 def applyEditGame(choice):
+    """  Processes edit game form rendering and form input processing. Unlike new game form this does not check the
+    number of players selected.
+
+        choice : int
+        Index to selected game (in list obtained from getAllgames(). Possible flaw if another user adds a new game for
+        te same team whilst in this workflow. (edge case)
+
+    """
     app.logger.debug("Entering applyEditGame()")
     games = ourDB.getAllGames()
     editGameDetails = ourDB.getGameDetailsForEditDeleteForm(games[choice].get("_id"), True)
@@ -309,11 +414,15 @@ def applyEditGame(choice):
                            )
 
 
-
 @app.route('/deletegame', methods=['POST'])
 @requires_auth
 @requires_managerRole
 def deleteGame():
+    """  Processes delete game select form (ie: which game to delete). As it has been redirected from a completed
+    form we should not get to the end of the function unless there are no games.
+ TO DO: Probably can delete the edit game form logic below.
+    """
+
     app.logger.debug("Entering deletegame")
     games = ourDB.getAllGames()
     gameLabels = formHandler.createLabelsForGames(games)
@@ -335,6 +444,13 @@ def deleteGame():
 @requires_auth
 @requires_managerRole
 def applyDeleteGame(choice):
+    """  Processes delete game form rendering and form input processing.
+
+        choice : int
+        Index to selected game (in list obtained from getAllGames(). Possible flaw if another user adds a new game for
+        te same team whilst in this workflow. (edge case)
+
+    """
     app.logger.debug("Entering applyDeleteGame")
     games = ourDB.getAllGames()
     dbid = games[choice].get("_id")
@@ -357,6 +473,9 @@ def applyDeleteGame(choice):
 @requires_auth
 @requires_managerRole
 def managePlayers():
+    """ Functionality to manage players - add, edit, retire and reactivate. The logic handles the response when adding a new player, but
+    edit, retire and reactivate players  are redirected to their endpoints via the form action setting in the managePlayers.html template.
+    """
     # add player, edit player, cannot delete player if they have played a game
     # dump players
     app.logger.debug("Entering managePlayers()")
@@ -385,6 +504,11 @@ def managePlayers():
 @requires_auth
 @requires_managerRole
 def editSelectPlayer():
+    """  Processes edit player select form (ie: which player to edit) and then redirects to edit player endpoint. Form
+    should always validate as endpoint is a post redirect from the managePlayers page.
+
+    TO DO: Remove GET method from function.
+    """
     app.logger.debug("We got to editSelectPlayer")
     allPlayers = ourDB.getAllPlayerDetailsForPlayerEdit()  # in obj classes
     playerList = formHandler.createLabelsForPlayers(ourDB.getAllPlayers(), action="allplayers")
@@ -404,6 +528,13 @@ def editSelectPlayer():
 @requires_auth
 @requires_managerRole
 def editPlayer(player):
+    """  Renders and post processes the edit player form for the selected player.
+
+        player : int
+        Index to selected player (in list obtained from createLabelsForPlayers() and removes selected player from list.
+        This is for validation to prevent a player name duplicate occurring during edit.
+
+    """
     app.logger.debug('Entering editPlayer')
     playerList = formHandler.createLabelsForPlayers(ourDB.getAllPlayers(), action="allplayers")
     ourPlayers = dict(playerList)
@@ -426,6 +557,11 @@ def editPlayer(player):
 @requires_auth
 @requires_managerRole
 def retirePlayer():
+    """  Processes retire player select form (ie: which player to retire). Form
+    should always validate as endpoint is a post redirect from the managePlayers page.
+
+    TO DO: Remove GET method from function.
+    """
     app.logger.debug('We got to retire player')
     allPlayers = ourDB.getAllPlayerDetailsForPlayerEdit()  # in obj classes
     playerList = formHandler.createLabelsForPlayers(ourDB.getAllPlayers(), action="retire")
@@ -448,6 +584,11 @@ def retirePlayer():
 @requires_auth
 @requires_managerRole
 def reactivatePlayer():
+    """  Processes reactivate (from retirement) player select form (ie: which player to reactivate). Form
+    should always validate as endpoint is a post redirect from the managePlayers page.
+
+    TO DO: Remove GET method from function.
+    """
     app.logger.debug('We got to reactivate player')
     playerList = formHandler.createLabelsForPlayers(ourDB.getAllPlayers(), action="reactivate")
     reactivatePlayerForm = formHandler.ReactivatePlayer()
@@ -469,6 +610,12 @@ def reactivatePlayer():
 @requires_auth
 @requires_managerRole
 def manageTransactions():
+    """ Functionality to manage transactions - add, edit, and show all. The logic handles the response when adding a new
+    transaction, but edit and list transactions are redirected to their endpoints via the form action setting in the
+    manageTransactions.html template. Note there is no functionality to remove transactions so any user errors require
+    additional transactions to adjust.
+
+    """
     # add payment, edit payment, remove payment, autopayquick, view all transactions
     # needs to sort on transaction from recent first.
     app.logger.debug('We got to transactions()')
@@ -496,6 +643,12 @@ def manageTransactions():
 @requires_auth
 @requires_managerRole
 def autoPay():
+    """  Processes thw autopay logic to automatically credit the manager (logged in user) with the value of the
+     last played game. Form should always validate as endpoint is a post redirect from the manageTransactions page.
+
+    TO DO: Remove GET method from function.
+    TO DO: Stop multiple actions for the same game to avoid accidental usage.
+    """
     app.logger.info('Got to autoPay()')
     quickAutoPayForm = formHandler.AutopayforCurrentUser()
 
@@ -512,6 +665,12 @@ def autoPay():
 @requires_auth
 @requires_managerRole
 def manageSettings():
+    """ Functionality to manage settings and similiar b adehavour including edit team name, export and import data, import
+     google sheet data and reset DB.  The logic handles the response when changing the team name  but the other actions
+     are redirected to their endpoints via the form action setting in the
+    manageSettings.html template. Note that import from json is not implemented yet.
+
+    """
     # edit team name, export and import db json for all collections\
     app.logger.debug("Got to manageSettings()")
     ourSettings = ourDB.getAppSettings()
@@ -539,6 +698,11 @@ def manageSettings():
 @requires_auth
 @requires_managerRole
 def downloadJSON():
+    """  Processes download of the DB in json format.  Form
+    should always validate as endpoint is a post redirect from the manageSettings page.
+
+    TO DO: Remove GET method from function.
+    """
     app.logger.debug(" we got to downloadJSON")
     # call to extract all tables as json and place into zip to download via sendfile
     filename = fileManager.exportarchive()
@@ -551,6 +715,10 @@ def downloadJSON():
 @requires_auth
 @requires_managerRole
 def uploadJSON():
+    """  Processes upload of the DB in json format.
+
+    TO DO: Implement. The False return value will generate an error.
+    """
     app.logger.debug("We got to uploadJSON()")
     return(False)
 
@@ -558,6 +726,12 @@ def uploadJSON():
 @requires_auth
 @requires_managerRole
 def uploadGoogleConnector():
+    """  Processes the completed google import form. Form
+    should always validate as endpoint is a post redirect from the manageSettings page.
+
+    TO DO: Remove GET method from function.
+    TO DO: Better error handling.
+    """
     app.logger.debug("We got to uploadGoogleConnector()")
 
     googleUploadForm = formHandler.PopulateFromGoogleSheet()
@@ -587,6 +761,13 @@ def uploadGoogleConnector():
 @requires_auth
 @requires_managerRole
 def deleteAllData():
+    """  Processes the DB deletion. Form
+    should always validate as endpoint is a post redirect from the manageSettings page.
+
+    TO DO: Remove GET method from function.
+    TO DO: Logic should drop the tenancy table but only the rows for the user's tenancy. Current behaviour aids
+    complete reset of DB and debugging/test cases.
+    """
     app.logger.warning("Entered deleteAllData() - about to reset DB")
     deleteAllForm = formHandler.DeleteAll()
     if deleteAllForm.validate_on_submit():
@@ -604,6 +785,11 @@ def deleteAllData():
 @requires_auth
 @requires_managerRole
 def manageUserAccess():
+    """ Functionality to manage user access including adding users and editing users. The logic handles the response
+    when adding a new user but editing user is redirected to that endpoint via the form action setting in the
+    manageUserAccess.html template.
+
+    """
     app.logger.debug("Got to manageUserAccess()")
     userAccessData = ourDB.getUserAccessData(session[constants.PROFILE_KEY].get('user_id', None))
     addAccessForm = formHandler.addAccess()
@@ -630,6 +816,14 @@ def manageUserAccess():
 @requires_auth
 @requires_managerRole
 def editSelectUser():
+    """  Renders and processes the edit User form for user access. Form
+    should always validate as endpoint is a post redirect from the manageUserAccess page.
+
+    TO DO: Remove GET method from function.
+    TO DO: There is no validation in the forms for duplicate user names/IDs etc
+    Big TO DO: Ideally integrate with a service that provides Auth0 functionality to obtain Auth0 IDs based on name/
+    email address etc.
+    """
     app.logger.debug("Got to editSelectUser()")
     userAccessData = ourDB.getUserAccessData(session[constants.PROFILE_KEY].get('user_id', None))
     addAccessForm = formHandler.addAccess()
@@ -648,6 +842,14 @@ def editSelectUser():
 @requires_auth
 @requires_managerRole
 def editUserAccess(user):
+    """  Renders and post processes the edit user access  form for the selected user.
+
+        player : int
+        Index to selected user (in list obtained from getUserAccessData().
+
+        Note: TO DO: No logic yet implemented to handle duplicate names in edit form.
+
+    """
     app.logger.debug('Entering editUserAccess')
     userAccessData = ourDB.getUserAccessData(session[constants.PROFILE_KEY].get('user_id', None))
     #ourUsers = dict(userAccessData)  # so we can turn index numbers into keys
@@ -668,6 +870,10 @@ def editUserAccess(user):
 @app.route('/playerSummary')
 @requires_auth
 def playerSummaryOnly():
+    """  Processes the page for player role users. This offers no form functionality but summary data of their accounts,
+    game activity and other stats. Also provides a bank statement style transaction view since they started playing.
+
+    """
     if ourDB.loadTeamTablesForUserId(session[constants.PROFILE_KEY].get('user_id', None)) == False:
         # this should not happen as this redirect page only occurs if the user_id exists already and is marked as a player.
         # as safety lets redirect to logout!
